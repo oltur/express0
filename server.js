77d6efd879599578d8ac01b5da565bfd444f2b7a
Alex Turevski <me@turevski.com>
2017-10-29 08:04:48 +0200

    v4

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world - version 3!\n');
+  res.send('Hello world - version 4!\n');
 });
 
 app.listen(PORT, HOST);
